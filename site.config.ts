6f09f907c15f25f41aef2d6a8c150302d11299bc
Antonio Molner <antoniomdk@hotmail.es>
2022-07-19 13:55:50 +0200

    Improve styling

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'c658ed7164a447848812a7ce395d1375',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -18,7 +18,6 @@ export default siteConfig({
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
   linkedin: 'fisch2',
 
   // newsletter: '#', // optional newsletter URL
